1a08d06247f0c422e371f61d3cfec7660f8fdf8a
michael <michaelgdimmitt@gmail.com>
2026-03-14 10:07:33 -0400

    feat: add generate_cask-aliases.sh to create shell aliases for installed brew casks

---
+++ generate_cask-aliases.sh
@@ -0,0 +1,10 @@
+# Run this once (or whenever you install/remove a cask)
+brew list --cask | while read cask; do
+  app=$(brew list --cask "$cask" 2>/dev/null | grep -m1 '\.app$' | xargs -I{} basename "{}" .app)
+  if [ -n "$app" ]; then
+    echo "alias $cask=\"open -a '$app'\""
+  fi
+done > ~/.brew-cask-aliases
+
+sed -i '' '/source ~\/.brew-cask-aliases/d' ~/.bashrc
+echo 'source ~/.brew-cask-aliases' >> ~/.bashrc
